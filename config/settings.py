293bb095c85d6827ae373f7bc5fe838147f65a1a
AbildayevTamerlan <tamerlanabildayev@gmail.com>
2026-09-07 23:47:45 +0500

    configure render logs

--- config/settings.py
+++ config/settings.py
@@ -146,5 +146,26 @@ STORAGES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://zeiin.onrender.com/",
+    "https://zeiin.onrender.com",
 ]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
